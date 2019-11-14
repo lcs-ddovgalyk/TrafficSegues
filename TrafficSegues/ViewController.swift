453382d8e657bd45ed0e1ce2c94b70218a522005
Dima Dovgalyk <ddovgalyk@lcs.on.ca>
2019-11-13 20:34:39 -0500

    Got programmatic segue for yellow and green buttons

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -10,24 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segueSwitch: UISwitch!
     
-    @IBOutlet weak var textField: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
         
     }
     
-    //Get ready to segue to the yellow view controller
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title =
-            textField.text
-        
+    @IBAction func greenButtonTapped(_ sender: Any) {
+
+        if segueSwitch.isOn{
+            performSegue(withIdentifier: "Green", sender: nil)
+
+        }
     }
+    
+    
 }
 
 
