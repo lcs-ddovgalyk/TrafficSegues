84f22254c7e4dd49301db6418dc54d069668ad5b
Dima Dovgalyk <ddovgalyk@lcs.on.ca>
2019-11-13 20:36:52 -0500

    Same as last commit, last commit had a mistake in it

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -20,7 +20,10 @@ class ViewController: UIViewController {
 
     
     @IBAction func yellowButtonTapped(_ sender: Any) {
-        
+        if segueSwitch.isOn{
+            performSegue(withIdentifier: "Yellow", sender: nil)
+
+        }
     }
     
     @IBAction func greenButtonTapped(_ sender: Any) {
